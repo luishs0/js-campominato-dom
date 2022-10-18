f9864205721ada0cf213eda7bb60739fd0aed490
luishs0 <luishs.doc@gmail.com>
2022-10-18 12:45:33 +0200

    cambiata la struttura

--- js/script.js
+++ js/script.js
@@ -3,51 +3,73 @@ let rndNumberIn;
 
 let grid = document.querySelector(".grid");
 
-let i = 1;
+let j = 1;
 while (numbers.length < 100) {
 
-    rndNumberIn = i;
+    rndNumberIn = j;
 
-    if (!numbers.includes(rndNumberIn)) {
-        numbers.push(rndNumberIn);
-    } 
-
-    i++
-   
+    numbers.push(rndNumberIn);
+    
+    j++
 }
 
-for (let i = 0; i < numbers.length; i++) {
+// CLICK PLAY
+
+let play = document.getElementById("play");
+
+
+play.addEventListener("click", function() {
+    grid.classList.add("d-flex");
+    grid.innerHTML = "";
     
-    let square = document.createElement("div");
-    square.classList.add("square");
 
-    square.innerHTML = numbers[i];
+    for (let i = 0; i < numbers.length; i++) {
+            
+        let square = document.createElement("div");
+        square.classList.add("square");
 
-    grid.append(square);
+        square.innerHTML = numbers[i];
+
+        grid.append(square);
     
-    square.addEventListener("click", function() {
-        this.classList.add("blue");
-        this.classList.add("color_white");
+        square.addEventListener("click", function() {
+            this.classList.add("blue");
+            this.classList.add("color_white");
+    
+            console.log(this.textContent);
+            
+        })
+        
+    }
 
-        console.log(this.textContent);
-    })
-
-    let play = document.getElementById("play");
-
-
-    play.addEventListener("click", function() {
-        grid.classList.add("d-flex");
-
-        if (square.classList.contains("blue")) {
-            square.classList.remove("blue");
-            square.classList.remove("color_white");
+    let arrayBombs = [];
+    while (arrayBombs.length < 16) {
+        let numberBomb = getRndInteger(1, 100);
+        if (!arrayBombs.includes(numberBomb)) {
+            arrayBombs.push(numberBomb);
         }
-    
-    })
-}
+    }
+    console.log(arrayBombs);
+        
+        
+})
+
 
 // FUNCTION
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
-  }
+}
+
+function squareBlue() {
+    square.classList.add("blue");
+    square.classList.add("color_white");
+
+    console.log(square.textContent);
+    if (square.classList.contains("blue")) {
+        square.classList.remove("blue");
+        square.classList.remove("color_white");
+    } else if (square.classList.contains("red")) {
+        square.classList.remove("red");
+    }
+}
